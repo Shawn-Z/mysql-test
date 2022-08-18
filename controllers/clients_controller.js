c6266fc03c869ea733b8f209efafc1d771e246bd
Shawn Zhou <45953653+Shawn-Z@users.noreply.github.com>
2022-08-17 19:04:51 -0700

    Update clients_controller.js

--- controllers/clients_controller.js
+++ controllers/clients_controller.js
@@ -1,15 +1,12 @@
-
 var express = require('express');
 var router = express.Router();
 var clients = require('../models/clientModel.js');
 var sections = require('../models/sectionsModel.js');
 var links = require('../models/linkModel.js');
 
-
 router.get('/', function (req, res) {
 	res.redirect('/clients');
 });
-
 
 router.get('/clients', function (req, res) {
 	let hbsObject = {};
@@ -28,13 +25,11 @@ router.get('/clients', function (req, res) {
 	});
 });
 
-
 router.post('/clients/create', function (req, res) {
 	clients.create('client_name', [req.body.name], function () {
 		res.redirect('/clients');
 	});
 });
-
 
 router.put('/clients/update/:clientId', function (req, res) {
 	var condition = 'clientId = ' + req.params.clientId;
@@ -44,7 +39,6 @@ router.put('/clients/update/:clientId', function (req, res) {
 	});
 });
 
-
 router.delete('/clients/delete/:clientId', function (req, res) {
 	var condition = 'clientId = ' + req.params.clientId; 
 	clients.delete(condition, function () {
@@ -52,14 +46,12 @@ router.delete('/clients/delete/:clientId', function (req, res) {
 	});
 });
 
-
 router.post('/sections/create', function (req, res) {
 
 	sections.create('sectionValue','clientId', {sectionValue: req.body.sectionValue}, {clientId: req.body.clientId}, function () {
 		res.redirect('/clients');
 	});
 });
-
 
 router.put('/sections/update/:sectionId', function (req, res) {
 	var condition = 'sectionId = ' + req.params.sectionId;
@@ -69,7 +61,6 @@ router.put('/sections/update/:sectionId', function (req, res) {
 	});
 });
 
-
 router.delete('/sections/delete/:sectionId', function (req, res) {
 	var condition = 'sectionId = ' + req.params.sectionId; 
 	sections.delete(condition, function () {
@@ -77,13 +68,11 @@ router.delete('/sections/delete/:sectionId', function (req, res) {
 	});
 });
 
-
 router.post('/links/create', function (req, res) {
 	links.create('linkName','sectionId', {linkName: req.body.linkName}, {sectionId: req.body.sectionId}, function () {
 		res.redirect('/clients');
 	});
 });
-
 
 router.put('/links/update/:linkId', function (req, res) {
 	var condition = 'linkId = ' + req.params.linkId;
@@ -92,13 +81,11 @@ router.put('/links/update/:linkId', function (req, res) {
 	});
 });
 
-
 router.delete('/links/delete/:linkId', function (req, res) {
 	var condition = 'linkId = ' + req.params.linkId; 
 	links.delete(condition, function () {
 		res.redirect('/clients');
 	});
 });
-
 
 module.exports = router;
